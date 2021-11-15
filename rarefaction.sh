ee42198d3d7df8ff10e7d106eabb07278eb90918
Chandler Sutherland <chandlersutherland@berkeley.edu>
2021-11-15 12:49:23 -0800

    rarefaction depth test

---
+++ rarefaction.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=rarefaction_vcf
+#SBATCH --account=ac_kvkallow
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=chandlersutherland@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
+
+module load python
+source activate nanopore
+
+IN_DIR=/global/scratch/users/chandlersutherland/
+TSTAMP=$(date +%Y%m%d-%H%M%S)
+
+depth=(200000 2000 1000 500 250 100 75 50 25)
+
+for i in "${depth[@]}"
+do 
+    echo "mpileup for depth $i"
+    bcftools mpileup \
+    -Ov \
+    -f $IN_DIR/phytozome/Athaliana/Araport11/assembly/Athaliana_447_TAIR10.fa \
+    -d $i \
+    -a FORMAT/AD,FORMAT/ADF,FORMAT/ADR,FORMAT/DP,FORMAT/SP,INFO/AD,INFO/ADF,INFO/ADR \
+    -r Chr1:26148811-26151472 \
+    --threads 20 $IN_DIR/20210831_pass_chopped.minimap2.sorted.sam |\
+    bcftools call \
+    -m -Ov \
+    --threads = 20	\
+    -o $IN_DIR/rarefaction/20210831_rarefaction_$i.vcf
+    echo "vcf file for depth $i created"
+done
