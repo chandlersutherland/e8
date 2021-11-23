fe5a3881eaa25b3f4b8e3c85fbf8a810c9b8dd3d
Chandler Sutherland <chandlersutherland@berkeley.edu>
2021-11-23 01:20:17 -0800

    added notes

--- guppy_sup_slurm.sh
+++ guppy_sup_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=sac_20211109_S3
-#SBATCH --account=ac_kvkallow
+#SBATCH --account=fc_kvkallow
 #SBATCH --partition=savio3_gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -10,6 +10,7 @@
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 
+#this script would allow for super accurate basecalling on the cluster using GPUs. However, only savio3_gpu has the appropriate video cards for nanopore basecalling and I only have access to gpu calling through fc_kvkallow
 nano_dir=/global/scratch/users/chandlersutherland/nanopore/20211109_CS/S3/20211109_1827_MN35019_AIH759_d0d0cd8b
 
 cd /global/home/users/chandlersutherland/programs/ont-guppy/bin
