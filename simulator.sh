f37d356d14811e22d7d901b93a4b8e63ef6ae151
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-05-08 18:47:28 -0700

    simulator slurm

---
+++ simulator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=simulator
+#SBATCH --account=ac_kvkallow
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=5:00:00
+#SBATCH --mail-user=chandlersutherland@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
+#SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
+
+module load python
+
+OUT_DIR=/global/scratch/users/chandlersutherland/mut_simulation
+
+cd $OUT_DIR
+python /global/home/users/chandlersutherland/e8/simulator.py
